b40e780f8f5fbfa8ae21bd21a5ad41ff208e55de
Joseph-N <jose@josephndungu.com>
2016-08-31 13:43:58 +0300

    COM-10 Automatically create an App

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -21,9 +20,10 @@ ActiveRecord::Schema.define(version: 20160818153256) do
     t.string   "name"
     t.text     "description"
     t.string   "uuid"
+    t.boolean  "default",     default: false
     t.integer  "user_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
     t.index ["user_id"], name: "index_apps_on_user_id", using: :btree
     t.index ["uuid"], name: "index_apps_on_uuid", unique: true, using: :btree
   end
