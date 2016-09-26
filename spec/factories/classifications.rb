10e3f2115d11fabb5a44764fae418b22bba0161c
Joseph-N <jose@josephndungu.com>
2016-09-26 11:34:11 +0300

    classifications WIP

--- spec/factories/classifications.rb
+++ spec/factories/classifications.rb
@@ -1,11 +1,13 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :app, aliases: [:added_by] do
-    name { Faker::App.name}
+  factory :classification do
+    name { Faker::App.name }
     description { Faker::Lorem.paragraph }
+    moderator
+    association :app
 
-    factory :invalid_app do
+    factory :invalid_classification do
       name nil
     end
   end
